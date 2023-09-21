4920e7754a7c188a25ee80ed352c60fc974de08c
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-09-21 14:43:23 +0900

    Initial commit

---
+++ client.tsx
@@ -0,0 +1,13 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './src/layouts/App';
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
