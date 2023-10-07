ce0d9988b13c8ba05e95a05ca7d78ee178e5171e
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-10-07 11:23:31 +0900

    feat : 리소스 수정
    feat : 리소스 수정

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 import { Container, ContainerCover } from '@styles/common';
 import { FooterStyle, IconCover, IconList, LogoCover, NavBar } from './style';
-import Logo_d from '@svg/shared/desktop/logo_light.svg';
-import Logo_t from '@svg/shared/desktop/logo_light_t.svg';
-import Logo_m from '@svg/shared/desktop/logo_light_m.svg';
-import Facebook from '@svg/shared/desktop/icon-facebook.svg';
-import Twitter from '@svg/shared/desktop/icon-twitter.svg';
-import Instagram from '@svg/shared/desktop/icon-instagram.svg';
+import Logo_d from '@svg/shared/logo_light.svg';
+import Logo_t from '@svg/shared/logo_light_t.svg';
+import Logo_m from '@svg/shared/logo_light_m.svg';
+import Facebook from '@svg/shared/icon-facebook.svg';
+import Twitter from '@svg/shared/icon-twitter.svg';
+import Instagram from '@svg/shared/icon-instagram.svg';
 import { useResponsive } from '@hooks/responsive';
 
 interface Props {}
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Container, ContainerCover } from '@styles/common';
 import Logo_d from '@svg/shared/logo_d.svg';
 import Logo_m from '@svg/shared/logo_m.svg';
-import Hamberger from '@svg/shared/mobile/icon-hamburger.svg';
+import Hamberger from '@svg/shared/icon-hamburger.svg';
 import { Content, HeaderStyle, LogoCover, NavBar } from './style';
 import { useResponsive } from '@hooks/responsive';
 
